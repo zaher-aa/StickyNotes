a034ea61dfbb441e8bf99f7b61110f44f16c4f78
zaher-aa <you@example.com>
2021-09-07 17:29:11 +0300

    All Dobne

---
+++ main.js
@@ -0,0 +1,76 @@
+const app = () => {
+  const input = document.querySelector("main .input-holder input");
+  const addBtn = document.querySelector("main .input-holder button");
+  const tasksParent = document.querySelector("main .all-tasks");
+  let tasksCount = document.querySelector(".tasks-count");
+  let comletedCount = document.querySelector(".completion-count");
+  let task;
+  // Add A Task
+  addBtn.onclick = function () {
+    let addableText = input.value;
+
+    // Check if the user has written something or not, if yes then add whatever he written otherwise do nothing
+    if (addableText.trim().length > 0) {
+      // If the "No Tasks To Show 'span'" element exists then remove it otherwise do nothing
+      if (document.querySelector(".all-tasks span")) {
+        document.querySelector(".all-tasks span").remove();
+      }
+      // Create task
+      task = document.createElement("div");
+      task.className = "task";
+
+      // Create textElement
+      let textElement = document.createElement("div");
+      textElement.className = "text";
+
+      // Create delElement
+      let delElement = document.createElement("button");
+      delElement.className = "delete";
+
+      // Create textElementText
+      let textElementText = document.createTextNode(addableText);
+      textElement.appendChild(textElementText);
+
+      // Create delElementText
+      let delElementText = document.createTextNode("Delete");
+      delElement.appendChild(delElementText);
+
+      // Append textElement to task
+      task.appendChild(textElement);
+      // Append delElement to task
+      task.appendChild(delElement);
+
+      // Append task to tatasksParentsk
+      tasksParent.appendChild(task);
+
+      // Remove all letters from the input field after adding the task
+      input.value = "";
+
+      // Increase the tasks count by one every time a task is added
+      tasksCount.innerHTML++;
+
+      // Delete a task
+      delElement.onclick = function () {
+        this.parentElement.remove();
+        tasksCount.innerHTML--;
+        if (tasksParent.childElementCount === 0) {
+          let noTasks = document.createElement("span");
+          let noTasksText = document.createTextNode("No Tasks To Show");
+          noTasks.appendChild(noTasksText);
+          tasksParent.appendChild(noTasks);
+        }
+      };
+    } else {
+      alert("You Should Write Something IN Order To Add It To The List.");
+    }
+
+    // Check a task
+    task.onclick = function () {
+      this.classList.toggle("done");
+      comletedCount.innerHTML =
+        document.querySelectorAll(".all-tasks .done").length;
+    };
+  };
+};
+
+app();
